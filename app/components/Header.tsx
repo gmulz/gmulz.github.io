9343255e5b0fdd805308fe91139e9f61e562dbb4
Grant Mulitz <themulitzer@gmail.com>
2024-12-15 01:39:42 +0100

    adding menu

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,13 +1,39 @@
-import { Flex, Heading, Text } from "@chakra-ui/react";
+import { Flex, Heading, Text, Button, Box } from "@chakra-ui/react";
+
+import {
+  MenuContent, MenuItem, MenuRoot, MenuTrigger, MenuTriggerItem,
+} from '@/components/ui/menu'
+
 
 
 export default function Header() {
   return (
-    <Flex align="start" w="100%" direction="row" marginBottom="5">
+    <Flex align="start" w="100%" direction="row" marginBottom="5" justify="space-between">
       <Flex direction="column">
-      <Heading color="green" fontSize="6xl" lineHeight="normal">Grant Mulitz</Heading>
+        <Heading color="green" fontSize="6xl" lineHeight="normal">Grant Mulitz</Heading>
         <Text color="green" fontFamily="subHeading">Comedy Writer &#8756; Engineer &#8756; Nerd</Text>
       </Flex>
+      
+      <MenuRoot>
+        <MenuTrigger asChild>
+          <Button>
+            Menu
+          </Button>
+        </MenuTrigger>
+        <MenuContent>
+          <MenuItem value="durr" >
+            About
+          </MenuItem>
+          <MenuRoot positioning={{ placement: 'left-end', gutter: 2}}>
+            <MenuTriggerItem value="writing">
+              Writing
+            </MenuTriggerItem>
+            <MenuContent>
+              <MenuItem value="clickhole">ClickHole</MenuItem>
+            </MenuContent>
+          </MenuRoot>
+        </MenuContent>
+      </MenuRoot>
       
     </Flex>
   )
