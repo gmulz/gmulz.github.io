a50218a10995df5bd685a5459fdf4a3b0165c962
Grant Mulitz <themulitzer@gmail.com>
2024-12-15 18:11:59 +0100

    hamburger menu with icon

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,46 +1,94 @@
-import { Flex, Heading, Text, Button } from "@chakra-ui/react";
+import { Flex, Heading, Text, Button, Icon } from "@chakra-ui/react";
+
+import { IoMenu } from "react-icons/io5";
 
 import {
-  MenuContent, MenuItem, MenuRoot, MenuTrigger, MenuTriggerItem,
-} from '@/components/ui/menu'
+  MenuContent,
+  MenuItem,
+  MenuRoot,
+  MenuTrigger,
+  // MenuTriggerItem,
+} from "@/components/ui/menu";
 
+type HeaderMenuItem = {
+  value: string;
+  title: string;
+};
 
+const menuItems: HeaderMenuItem[] = [
+  {
+    title: "About",
+    value: "about",
+  },
+  {
+    title: "Writing",
+    value: "writing",
+  },
+  {
+    title: "Sketch Comedy",
+    value: "sketch-comedy",
+  },
+  {
+    title: "Software Engineering",
+    value: "tech",
+  },
+];
+
+const createMenuItem = (obj: HeaderMenuItem) => {
+  return (
+    <MenuItem
+      value={obj.value}
+      backgroundColor="grey"
+      color="green"
+      padding="2"
+      asChild
+    >
+      <a href={`/${obj.value}`}>{obj.title}</a>
+    </MenuItem>
+  );
+};
 
 export default function Header() {
   return (
-    <Flex 
+    <Flex
       align="start"
-       w="100%" 
-       direction="row" 
-       marginBottom="5" 
-       justify="space-between"
-       alignItems="center">
-      <Flex direction="column">
-        <Heading color="green" fontSize="6xl" lineHeight="normal">Grant Mulitz</Heading>
-        <Text color="green" fontFamily="subHeading">Comedy Writer &#8756; Engineer &#8756; Nerd</Text>
-      </Flex>
-      
-      <MenuRoot positioning={{ placement: "bottom-end"}}>
+      w="100%"
+      direction="row"
+      marginBottom="5"
+      justify="space-between"
+      alignItems="center"
+    >
+      <a href="/">
+        <Flex direction="column">
+          <Heading color="green" fontSize="6xl" lineHeight="normal">
+            Grant Mulitz
+          </Heading>
+          <Text color="green" fontFamily="subHeading">
+            Comedy Writer &#8756; Engineer &#8756; Nerd
+          </Text>
+        </Flex>
+      </a>
+
+      <MenuRoot positioning={{ placement: "bottom-end" }}>
         <MenuTrigger asChild>
-          <Button padding="4" color="green" backgroundColor="grey">
-            Menu
+          <Button padding="2" color="green" backgroundColor="grey">
+            <Icon fontSize="3xl">
+              <IoMenu />
+            </Icon>
           </Button>
         </MenuTrigger>
         <MenuContent background="grey">
-          <MenuItem value="durr" backgroundColor="grey" color="green" padding="2">
-            About
-          </MenuItem>
-          <MenuRoot positioning={{ placement: 'left-end', gutter: 0}}>
+          {menuItems.map(createMenuItem)}
+          {/* <MenuRoot positioning={{ placement: 'left-end', gutter: 0}}>
             <MenuTriggerItem value="writing" backgroundColor="grey" color="green" padding="2">
               Writing
             </MenuTriggerItem>
             <MenuContent background="grey">
               <MenuItem value="clickhole" backgroundColor="grey" color="green" padding="2">ClickHole</MenuItem>
             </MenuContent>
-          </MenuRoot>
+          </MenuRoot> */}
         </MenuContent>
       </MenuRoot>
-      
     </Flex>
-  )
+  );
 }
--- app/writing/clickhole/page.tsx
+++ app/writing/clickhole/page.tsx
@@ -1,5 +1,5 @@
-import { Flex } from "@chakra-ui/react"
+import { Flex } from "@chakra-ui/react";
 
 export default function ClickHole() {
-  return <></>
+  return <></>;
 }
